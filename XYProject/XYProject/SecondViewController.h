a3d0f327cfa3a24ae47742439eb7491e22521ce1
zhaoxuyang <xuyanggoodluck@126.com>
2016-02-16 00:23:15 +0800

    add second vc

---
+++ XYProject/XYProject/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  XYProject
+//
+//  Created by 赵旭阳 on 16/2/16.
+//  Copyright © 2016年 zhaoxuyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
